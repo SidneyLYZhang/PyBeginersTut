e25d8cf7ce1cb82b866bde2adecf425ec36c2e28
Sidney Zhang <liangyi@me.com>
2024-03-12 18:20:39 +0800

    20240312-Update

--- PybeCode/__init__.py
+++ PybeCode/__init__.py
@@ -2,5 +2,6 @@ from .funcs import randoms
 from .funcs import random_number
 from .funcs import random_words
 from .funcs import random_Lorem
+from .funcs import birthday_random
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
